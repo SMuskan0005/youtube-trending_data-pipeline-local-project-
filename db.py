3d6bbe52326698e0ff56e7e92dc474e38b61cd93
SMuskan0005 <shah09muskan@gmail.com>
2025-07-30 22:11:14 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,8 +2,8 @@ import psycopg2
 def get_connection():
     return psycopg2.connect(
         dbname='yt_data',
-        user="yt_user",
-        password="yt_password",
+        user="yt_user or whatever suits you",
+        password="yt_pass or whatever suits you",
         host="localhost",
         port="5432"
     )
